5b474aee810a6c58be291667c9503e356f17482e
Rajkiran Gurunathan (TA-14) <Rajkiran.GA.Gurunathan@bmw.de>
2020-04-19 11:37:55 +0200

    add addToCart button and details of the product

--- src/components/Details.js
+++ src/components/Details.js
@@ -1,21 +1,64 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { ProductConsumer } from "../context";
+import { Link } from "react-router-dom"
 
 class Details extends Component {
-    constructor(props) {
-        super(props)
-
-        this.state = {
-                 
-        }
-    }
-
-    render() {
-        return (
-            <div>
-                <h3>details title</h3>
+  render() {
+    return (
+      <ProductConsumer>
+        {(value) => {
+          var id, img, info, price, title, inCart;
+          value.detailProduct.map((item) => {
+            (id = item.id),
+              (title = item.title),
+              (img = item.img),
+              (info = item.info),
+              (price = item.price),
+              (inCart = item.inCart);
+          });
+          return (
+            <div className="container py-5">
+              <div className="row">
+                <div className="col-10 mx-auto my-5">
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-10 mx-auto col-md-6 my-3">
+                  <img src={img} className="img-fluid" alt="product" />
+                </div>
+                <div className="col-10 mx-auto col-md-6 my-3">
+                  <h2>Pizza : {title}</h2>
+                  <h4>
+                    <strong>
+                      price : <span>$</span> {price}
+                    </strong>
+                  </h4>
+                  <p className=" mt-3 mb-0">
+                    {" "}
+                    some info about pizza:
+                  </p>
+                  <p>{info}</p>
+                  <div>
+                    <Link to="/">
+                      <button>back to products</button>
+                    </Link>
+                    <button
+                      disabled={inCart ? true : false}
+                      onClick={() => {
+                        console.log(("you clicked the button"));
+                      }}
+                    >
+                      {inCart ? "inCart" : "add to Cart"}
+                    </button>
+                  </div>
+                </div>
+              </div>
             </div>
-        )
-    }
+          );
+        }}
+      </ProductConsumer>
+    );
+  }
 }
 
-export default Details
+export default Details;
--- src/components/Product.js
+++ src/components/Product.js
@@ -1,21 +1,48 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import { Link } from "react-router-dom";
+import { ProductConsumer } from "../context";
 
 class Product extends Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props);
 
-        this.state = {
-                 
-        }
-    }
+    this.state = {};
+  }
 
-    render() {
-        return (
-            <div>
-                <h3>product title</h3>
-            </div>
-        )
-    }
+  render() {
+    const { img, inCart } = this.props.product;
+    console.log(inCart)
+
+    return (
+      <div className="col-9 mx-auto col-md-6 col-lg-3 my-3">
+        <div className="card">
+          <div
+            className="img-container p-5"
+            onClick={() => console.log("you clicked the image")}
+          >
+            <Link to="/details">
+              <img src={img} alt="product" className="card-img-top" />
+            </Link>
+            <button
+              className="cart-btn"
+              disabled={inCart ? true : false}
+              onClick={() =>console.log("you clicked the cart")
+              }
+            >
+              {inCart ? (
+                <p className="text-capitalize mb-0" disabled>
+                  {""}
+                  inCart
+                </p>
+              ) : (
+                <i className="fas fa-cart-plus"></i>
+              )}
+            </button>
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
-export default Product
+export default Product;
